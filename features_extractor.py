85fff5a603b2a5fb1bfc16f874549bfc6224727d
Jacopo Zagoli <jacopo.zagoli@studenti.univr.it>
2023-02-07 10:23:44 +0100

    added number of conflicted agents couples feature

--- features_extractor.py
+++ features_extractor.py
@@ -1,7 +1,7 @@
 # Jacopo Zagoli, 31/01/2023
 from utils import ravel
 from grid_solver import GridSolver
-from conflicts import ConflictsFinder
+from conflicts import ConflictsFinder, Conflict
 
 
 class FeaturesExtractor:
@@ -14,45 +14,53 @@ class FeaturesExtractor:
         self.__paths = self.__compute_paths()
         self.__conflicts = self.__compute_conflicts()
 
-    def __compute_paths(self):
+    def __compute_paths(self) -> list:
         paths = []
         for waypoints in self.__assignment:
             path = self.__grid_solver.get_waypoints_path(waypoints)
             paths.append(path)
         return paths
 
-    def __compute_conflicts(self):
+    def __compute_conflicts(self) -> list[Conflict]:
         finder = ConflictsFinder(self.__paths)
         return finder.get_conflicts()
 
-    def get_features(self):
+    def get_features(self) -> list[int]:
         features = []
         features += self.__agents_start_goal()
         features += self.__n_waypoints_per_agent()
         features += self.__paths_length()
         features += self.__n_conflicts()
         features += self.__n_conflicted_agents()
+        features += self.__n_conflicted_agent_couples()
         return features
 
-    def __agents_start_goal(self) -> list:
+    def __agents_start_goal(self) -> list[int]:
         n_cols = self.__grid_size[1]
         locations = []
         for waypoints in self.__assignment:
             locations += [waypoints[0], waypoints[-1]]
         return [ravel(p, n_cols) for p in locations]
 
-    def __n_waypoints_per_agent(self) -> list:
+    def __n_waypoints_per_agent(self) -> list[int]:
         return [len(waypoints) for waypoints in self.__assignment]
 
-    def __paths_length(self) -> list:
+    def __paths_length(self) -> list[int]:
         return [len(path) for path in self.__paths]
 
-    def __n_conflicts(self) -> list:
+    def __n_conflicts(self) -> list[int]:
         return [len(self.__conflicts)]
 
-    def __n_conflicted_agents(self) -> list:
+    def __n_conflicted_agents(self) -> list[int]:
         conflicted_agents = set()
         for c in self.__conflicts:
             conflicted_agents.add(c.first_agent)
             conflicted_agents.add(c.second_agent)
         return [len(conflicted_agents)]
+
+    def __n_conflicted_agent_couples(self) -> list[int]:
+        # in conflicts list, conflicts are ordered by increasing first agent
+        couples = set()
+        for conflict in self.__conflicts:
+            couples.add((conflict.first_agent, conflict.second_agent))
+        return [len(couples)]
